994e9c13de92e32cf1613853d9fbdcda3cf19aeb
Francis Ong <francis.ong25@gmail.com>
2022-05-02 15:34:17 +0800

    add new rows for other genres

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         fetchURL={requests.fetchNetflixOriginals}
       />
       <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanticMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
