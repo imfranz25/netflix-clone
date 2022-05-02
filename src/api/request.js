0a340a103d7be0484957fee1432407849eae6c59
Francis Ong <francis.ong25@gmail.com>
2022-05-02 10:15:18 +0800

    add request api shorthand

---
+++ src/api/request.js
@@ -0,0 +1,16 @@
+// UNIQUE KEY FOR THE API REQUEST (SHORTHANDED VARIABLE)
+const API_KEY = "8f4647f87684c2da74470dcad480b0f3";
+
+// REQUESTS FOR GENRES IN A OBJECT FORMAT
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanticMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
