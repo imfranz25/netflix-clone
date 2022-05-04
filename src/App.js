94ff1c3492868a717ec45e3e73b2c50ef792064a
Francis Ong <francis.ong25@gmail.com>
2022-05-04 10:08:47 +0800

    add function for setting bg in dashboard page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React, { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 //PAGES
+import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 // STYLES
 import "./css/App.scss";
 
 function App() {
+  const [isAuth, setAuth] = useState(false);
+
+  const SetBGDashboard = () => {
+    const location = useLocation();
+    return location.pathname === "/dashboard";
+  };
+
   return (
-    <div className="app">
-      <Dashboard />
+    <div className={SetBGDashboard() && "app"}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>hahaha hahaha haha</div>;
+}
+
+export default Home;
