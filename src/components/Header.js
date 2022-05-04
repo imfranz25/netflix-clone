04237de836dc11c5afe10759cd5448de0ede4cb5
Francis Ong <francis.ong25@gmail.com>
2022-05-04 17:40:47 +0800

    add assets (img, vids), add content in main section, add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,22 @@ import "./../css/Header.scss";
 
 function Header() {
   return (
-    <header class="showcase">
-      <nav class="showcase-top">
+    <header className="showcase">
+      <nav className="showcase-top">
         <img
           id="logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158"
         />
-        <button class="submit" type="submit">
+        <button className="submit" type="submit">
           Sign In
         </button>
       </nav>
-      <div class="get-started-container">
+      <div className="get-started-container">
         <h1>
           Unlimited movies, TV <br /> shows, and more.
         </h1>
-        <p class="para-1">Watch anywhere. Cancel anytime.</p>
-        <p class="para-2">
+        <p className="para-1">Watch anywhere. Cancel anytime.</p>
+        <p className="para-2">
           Ready to watch? Enter your email to create or restart your membership.
         </p>
         <input type="text" name="email" placeholder="Email Address" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,88 @@
 import React from "react";
 import Header from "./../components/Header";
+// STYLE
+import "./../css/Home.scss";
+// ASSETS
+import tv__img from "./../assets/images/tv.png";
+import video__1 from "./../assets/videos/video1.m4v";
+import dl__img from "./../assets/images/download.jpg";
+import box__shot from "./../assets/images/boxshot.png";
+import video__2 from "./../assets/videos/video2.m4v";
+import comp__img from "./../assets/images/computer.png";
+import profile__img from "./../assets/images/create_profile.png";
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <Header />
+      <main className="main-section">
+        <div className="section-container">
+          <div className="left-column">
+            <h1>Enjoy on your TV.</h1>
+            <h3>
+              Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV,
+              Blu-ray players, and more.
+            </h3>
+          </div>
+          <div className="right-column">
+            <img src={tv__img} alt="Netflix TV" />
+            <video className="video-1" autoPlay playsInline muted loop>
+              <source src={video__1} type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        <div className="section2-container">
+          <div className="left-column">
+            <img src={dl__img} alt="Netflix Download" />
+            <div className="box">
+              <img className="boxshot" src={box__shot} alt="Netflix Box Shot" />
+              <p>
+                Stranger Things
+                <br />
+                <a href="#">Downloading...</a>
+              </p>
+              <img
+                className="download-gif"
+                src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/download-icon.gif"
+                alt="Netflix Download"
+              />
+            </div>
+          </div>
+          <div className="right-column">
+            <h1>Download your shows to watch offline.</h1>
+            <h3>
+              Save your favorites easily and always have something to watch.
+            </h3>
+          </div>
+        </div>
+        <div className="section-container">
+          <div className="left-column">
+            <h1>Watch everywhere.</h1>
+            <h3>
+              Stream unlimited movies and TV shows on your phone, tablet,
+              laptop, and TV without paying more.
+            </h3>
+          </div>
+          <div className="right-column">
+            <img src={comp__img} alt="Netflix Computer" />
+            <video className="video-2" autoPlay playsInline muted loop>
+              <source src={video__2} type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        <div className="section2-container">
+          <div className="left-column">
+            <img src={profile__img} alt="Create Profile" />
+          </div>
+          <div className="right-column">
+            <h1>Create profiles for kids.</h1>
+            <h3>
+              Send kids on adventures with their favorite characters in a space
+              made just for them—free with your membership.
+            </h3>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
