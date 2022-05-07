259baecc4563c61bc567cb1f5ee743bf797855bb
Francis Ong <francis.ong25@gmail.com>
2022-05-07 10:42:27 +0800

    add validation in inputs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,7 +9,9 @@ import "./../css/LoginForm.scss";
 
 function LoginForm({ setAuth }) {
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
   const [pass, setPass] = useState("");
+  const [passError, setpassError] = useState(false);
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePass = (e) => setPass(e.target.value);
   const navigate = useNavigate();
@@ -18,38 +20,66 @@ function LoginForm({ setAuth }) {
    * pass: 1234
    */
   const checkUser = () => {
-    if (email === "bayanacademy@gmail.com" && pass === "1234") {
-      setAuth(true);
-      alert("Login Success");
-      navigate("/dashboard");
-    } else {
-      alert("Login Failed, please try again !");
+    setEmailError(email.length === 0);
+    setpassError(pass.length === 0);
+    // VALIDATE ONCE EMAIL AND PASS IS SPECIFIED
+    if (email.length > 0 && pass.length > 0) {
+      if (email === "bayanacademy@gmail.com" && pass === "1234") {
+        setAuth(true);
+        alert("Login Success");
+        navigate("/dashboard");
+      } else {
+        alert("Login failed, invalid email or password");
+      }
     }
   };
 
   return (
     <div className="login-form-container">
-      <Box className="form-box" autoComplete="off">
+      <Box className="form-box">
         <h1>Sign In</h1>
         <div className="input-container">
           <TextField
+            autoComplete="off"
             InputLabelProps={{ style: { color: "white" } }}
-            sx={{ input: { color: "white" } }}
+            sx={{
+              input: {
+                color: "white",
+                background: "rgba(20, 20, 20, 0.8)",
+                borderRadius: "5px",
+              },
+            }}
             label="Email or Phone Number"
             variant="filled"
             value={email}
             onChange={handleEmail}
+            onBlur={() => setEmailError(email.length === 0)}
+            error={emailError && email.length === 0}
+            helperText={
+              emailError && email.length === 0 ? "Please enter your email" : ""
+            }
           />
         </div>
         <div className="input-container">
           <TextField
             InputLabelProps={{ style: { color: "white" } }}
-            sx={{ input: { color: "white" } }}
+            sx={{
+              input: {
+                color: "white",
+                background: "rgba(20, 20, 20, 0.8)",
+                borderRadius: "5px",
+              },
+            }}
             type="password"
             label="Password"
             variant="filled"
             value={pass}
             onChange={handlePass}
+            onBlur={() => setpassError(pass.length === 0)}
+            error={passError && pass.length === 0}
+            helperText={
+              passError && pass.length === 0 ? "Please enter your password" : ""
+            }
           />
         </div>
         <div className="sign-container">
